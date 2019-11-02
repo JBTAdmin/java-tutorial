84da438894ac31a23307000562a962e4966742d2
JBT <ADMIN@JAVABEGINNERSTUTORIAL.COM>
2019-11-02 15:12:38 +0530

    Java Tutorial for Switch statement example

--- switch_statement.java
+++ switch_statement.java
@@ -2,8 +2,14 @@
 public class switch_statement {
 
     public static void main(String[] args) {
-        boolean isWeekend = false;
         int weekday = 5;
+        boolean isWeekend = false;
+
+        SwitchIntMethod(weekday);
+        switchBreakUse(isWeekend, weekday);
+    }
+
+    private static void switchBreakUse(boolean isWeekend, int weekday) {
         switch (weekday) {
             /*
              * Two cases can be combined in different ways.
@@ -24,5 +30,37 @@ public class switch_statement {
                 isWeekend = false;
         }
         System.out.println("Is it Weekend ? " + isWeekend);
+    }
+
+    private static void SwitchIntMethod(int weekday) {
+        String weekdayString;// switch statement with int data type
+        switch (weekday) {
+            case 1:
+                weekdayString = "Sunday";
+                break;
+            case 2:
+                weekdayString = "Monday";
+                break;
+            // default statement can be anywhere. Not necessarily in the end only
+            default:
+                weekdayString = "Not a weekday";
+                break;
+            case 3:
+                weekdayString = "Tuesday";
+                break;
+            case 4:
+                weekdayString = "Wednesday";
+                break;
+            case 5:
+                weekdayString = "Thursday";
+                break;
+            case 6:
+                weekdayString = "Friday";
+                break;
+            case 7:
+                weekdayString = "Saturday";
+                // break is not required for last case
+        }
+        System.out.println(weekdayString);
     }
 }
